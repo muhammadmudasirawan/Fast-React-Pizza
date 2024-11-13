acfc5678be8031ca5cb86234a76541f21aa7d827
Muhammad Mudasir <167820839+muhammadmudasirawan@users.noreply.github.com>
2024-11-13 23:23:46 +0500

    change carditem.jsx

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,17 +1,14 @@
-
-import { useSelector } from 'react-redux';
-import { formatCurrency } from '../../utils/helpers';
-import { getCurrentQuantityById } from './cartSlice';
-import DeleteItem from './deleteItem';
-import UpdataItemQuantity from './UpdataItemQuantity';
-
-
+import { useSelector } from 'react-redux'
+import { formatCurrency } from '../../utils/helpers'
+import { getCurrentQuantityById } from './cartSlice'
+import UpdataItemQuantity from './UpdataItemQuantity'
+import DeleteItem from './DeleteItem'
 
 function CartItem({ item }) {
-  const { pizzaId, name, quantity, totalPrice } = item;
+  const { pizzaId, name, quantity, totalPrice } = item
 
-  const currentQuantity = useSelector(getCurrentQuantityById(pizzaId));
-  console.log(currentQuantity);
+  const currentQuantity = useSelector(getCurrentQuantityById(pizzaId))
+  console.log(currentQuantity)
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -20,15 +17,17 @@ function CartItem({ item }) {
       </p>
 
       <div className="flex items-center justify-between sm:gap-6">
-
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
 
-<UpdataItemQuantity pizzaId={pizzaId} currentQuantity={currentQuantity} />
+        <UpdataItemQuantity
+          pizzaId={pizzaId}
+          currentQuantity={currentQuantity}
+        />
 
-       <DeleteItem  pizzaId={pizzaId } />
+        <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
-  );
+  )
 }
 
-export default CartItem;
+export default CartItem
